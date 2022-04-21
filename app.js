5b5cacab6a729f7d1fe6808733eb4da39d332e18
Nathan Bonilla <nbonilla409@gmail.com>
2022-04-20 18:48:33 -0700

    built portfolio foundation

---
+++ app.js
@@ -0,0 +1,4 @@
+const track = document.querySelector('.carousel_track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel_button-right')
+const prevButton = document.querySelector('.carousel_button-left')
